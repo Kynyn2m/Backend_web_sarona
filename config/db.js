ff52b9a760afb3f01af11bbb7ca3f109447c1780
Nom Kiny <kinynom@gmail.com>
2024-05-21 09:51:30 +0700

    updated DB

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "12345678",
-  database: process.env.DB_NAME || "mywebapp",
+  database: process.env.DB_NAME || "sarona",
 });
 
 db.connect((err) => {
--- database.js
+++ database.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "12345678",
-  database: process.env.DB_NAME || "mywebapp",
+  database: process.env.DB_NAME || "sarona",
 });
 
 connection.connect((err) => {
